a9c88e89c846794dbb2ffba4688a6dcaedaf6685
Emmanuel Ungab <john.ungab7@gmail.com>
2026-06-10 15:30:35 +0800

    edit: change color theme

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       require("bufferline").setup({
         options = {
-          mode = "buffers",
           offsets = {
             {
               filetype = "neo-tree",
@@ -14,20 +13,61 @@ return {
               highlight = "NeoTreeOffsetLabel",
             },
           },
-          separator_style = "thin",
-          modified_icon = "● ",
-          indicator = { style = "none" },
-          show_close_icon = false,
-          show_buffer_close_icons = false,
           diagnostics = "nvim_lsp",
-          show_buffer_icons = true,
-          always_show_bufferline = true,
+          separator_style = "thin",
+          indicator = { style = "none" },
+          show_buffer_close_icons = false,
+          modified_icon = "●",
+          show_close_icon = false,
+          show_tab_indicators = false,
           max_name_length = 18,
           truncate_names = true,
           tab_size = 18,
         },
+        highlights = {
+          fill = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLineNC" },
+          },
+          background = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLine" },
+          },
+          buffer_visible = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+          },
+          buffer_selected = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+          },
+          separator = {
+            fg = { attribute = "bg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLine" },
+          },
+          separator_selected = {
+            fg = { attribute = "fg", highlight = "Special" },
+            bg = { attribute = "bg", highlight = "Normal" },
+          },
+          separator_visible = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLineNC" },
+          },
+          close_button = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLine" },
+          },
+          close_button_selected = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+          },
+          close_button_visible = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+          },
+        },
       })
-      vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "#292c3c" })
+      vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "#1e1e1e" })
     end,
   },
 }
--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,28 +1,21 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  priority = 1000,
-  name = "catppuccin",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    flavour = "frappe",
-    transparent_background = false,
-    term_colors = true,
-    integrations = {
-      telescope = true,
-      noice = true,
-      lualine = true,
-      native_lsp = { enabled = true },
-      treesitter = true,
-      indent_blankline = { enabled = true },
-      which_key = true,
-      cmp = true,
-      bufferline = true,
-      notify = true,
-    },
-  },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	"Mofiqul/vscode.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		local c = require("vscode.colors").get_colors()
+		require("vscode").setup({
+			transparent = false,
+			italic_comments = true,
+			italic_inlayhints = true,
+			underline_links = true,
+			disable_nvimtree_bg = false,
+			terminal_colors = true,
+			color_overrides = {},
+			group_overrides = {
+				Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+			},
+		})
+		vim.cmd.colorscheme("vscode")
+	end,
 }
